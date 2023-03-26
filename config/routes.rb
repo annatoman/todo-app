4197a2c45e7374da226c3398b88da674ce9bd6bf
Anna <annamtoman@gmail.com>
2023-03-26 16:37:48 -0500

    adds user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-
+  
 resources :users
 resources :categories
 resources :todos
+
+get "/login" => "sessions#new"  
+post "/sessions" => "sessions#create"
+get "/logout" => "sessions#destroy"
 
 end
